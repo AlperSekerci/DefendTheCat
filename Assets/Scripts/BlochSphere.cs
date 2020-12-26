96497e9427b65934667a8be7039c218351c3a42d
Alper Sekerci <alprsekerci@gmail.com>
2020-12-26 10:30:14 +0300

    Spherical coordinates to rect.

--- Assets/Scripts/BlochSphere.cs
+++ Assets/Scripts/BlochSphere.cs
@@ -2,10 +2,31 @@
 
 public class BlochSphere : MonoBehaviour
 {
+    public const float PI2 = Mathf.PI * 2;
     public float rotateSpeed = 100;
+    public float theta = 0;
+    public float phi = 0;
+    public Transform pointTM;
 
     private void Update()
     {
+        UpdatePoint(theta, phi);
         transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+    }
+
+    // Source: http://www.vcpc.univie.ac.at/~ian/hotlist/qc/talks/bloch-sphere.pdf
+    public void UpdatePoint(float theta, float phi)    
+    {
+        theta = Mathf.Clamp(theta, 0, Mathf.PI);
+        phi = Mathf.Clamp(phi, 0, PI2);
+        Vector3 point = SphericalCoordToRect(theta, phi);
+        pointTM.localPosition = point / transform.localScale.x;
+    }
+
+    // Source: https://blog.nobel-joergensen.com/2010/10/22/spherical-coordinates-in-unity/#:~:text=Spherical%20coordinate%20system%20is%20an,the%20origin%20to%20the%20point
+    private Vector3 SphericalCoordToRect(float theta, float phi)
+    {
+        float a = Mathf.Cos(phi);      
+        return new Vector3(a * Mathf.Sin(theta), a * Mathf.Cos(theta), Mathf.Sin(phi));
     }
 }
---
+++ qiskit/defend_the_cat.py
@@ -0,0 +1,43 @@
+# Edited the code I wrote for assignment 2.
+from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit, execute, Aer
+
+bits = ['0', '1']
+all_inputs = []
+for a in bits:
+    for b in bits:
+        for c in bits:
+            for d in bits:
+                all_inputs.append(a + b + c + d)
+
+def color2(qreg, circuit):
+    # (a)
+    circuit.cx(qreg[0], qreg[5])
+    circuit.cx(qreg[2], qreg[5])
+    # (b)
+    circuit.cx(qreg[0], qreg[6])
+    circuit.cx(qreg[3], qreg[6])
+    # (c)
+    circuit.cx(qreg[1], qreg[7])
+    circuit.cx(qreg[2], qreg[7])
+    # (d)
+    circuit.ccx(qreg[5], qreg[6], qreg[8])
+    circuit.ccx(qreg[7], qreg[8], qreg[4])
+
+for input in all_inputs:
+    qreg = QuantumRegister(9)
+    creg = ClassicalRegister(1)
+    circuit = QuantumCircuit(qreg, creg)
+
+    # initialize the inputs
+    for i in range(4):
+        if input[i] == '1':
+            circuit.x(qreg[3 - i])
+
+    color2(qreg, circuit)
+    circuit.measure(qreg[4], creg)
+    # print(circuit.draw(output='text'))
+
+    job = execute(circuit, Aer.get_backend('qasm_simulator'), shots=1)
+    counts = job.result().get_counts(circuit)
+    for outcome in counts:
+        print("(input vertices) |{}> --> |{}> (output)".format(input, outcome))
