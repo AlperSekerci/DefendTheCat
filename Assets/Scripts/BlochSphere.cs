2ad73971b8e6f37a08488ed0918362187a870878
Alper Sekerci <alprsekerci@gmail.com>
2020-12-26 09:47:26 +0300

    Initial commit

---
+++ Assets/Scripts/BlochSphere.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class BlochSphere : MonoBehaviour
+{
+    public float rotateSpeed = 100;
+
+    private void Update()
+    {
+        transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+    }
+}
